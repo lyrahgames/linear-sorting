fedfea69e96fc6eb18e61d2c24e9d8100a300f3f
lyrahgames <markuspawellek@gmail.com>
2019-02-08 10:50:42 +0100

    Add automatic histogram size for counting sort

--- counting_sort.cc
+++ counting_sort.cc
@@ -13,25 +13,35 @@ ostream& operator<<(ostream& os, const vector<T>& v) {
   return os << ")";
 }
 
-void counting_sort(vector<int>::iterator first, vector<int>::iterator last,
-                   vector<int>::iterator out) {
-  constexpr int size = 10;
+template <typename ForwardIt, typename RandomIt>
+void counting_sort(ForwardIt first, ForwardIt last, RandomIt out) {
+  auto it = first;
+  int min = *it;
+  int max = *it;
+  for (; it != last; ++it) {
+    max = std::max(max, *it);
+    min = std::min(min, *it);
+  }
+
+  const int offset = min;
+  const int size = max - min + 1;
+
   int counts[size]{};
-  for (auto it = first; it != last; ++it) ++counts[*it];
+  for (auto it = first; it != last; ++it) ++counts[*it - offset];
 
   int sum = counts[0];
   counts[0] = 0;
   for (int i = 1; i < size; ++i) {
-    int tmp = counts[i];
-    counts[i] = sum;
-    sum += tmp;
+    int tmp = sum;
+    sum += counts[i];
+    counts[i] = tmp;
   }
 
-  for (auto it = first; it != last; ++it) out[counts[*it]++] = *it;
+  for (auto it = first; it != last; ++it) out[counts[*it - offset]++] = *it;
 }
 
 TEST_CASE("Counting Sort") {
-  vector<int> in{9, 5, 1, 5, 2, 3, 5, 1, 5, 4, 7};
+  vector<int> in{0, 9, 5, 1, 5, -10, 2, 3, 5, 1, 5, 4, 7, 25};
   vector<int> out(size(in));
 
   auto test = in;
